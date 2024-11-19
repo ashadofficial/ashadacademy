60936787945e3be18d7e96b7b4e29247c41d5988
ashadofficial <mdashadullah608@gmail.com>
2024-11-19 22:40:19 +0900

    Add files via upload

---
+++ .github/as.js
@@ -0,0 +1,5 @@
+// JavaScript for button click interaction
+document.getElementById("message-btn").addEventListener("click", function() {
+    var message = document.getElementById("message");
+    message.style.display = "block"; // Show the hidden message
+});
